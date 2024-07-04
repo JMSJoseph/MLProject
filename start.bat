657fa98130653964d3e1d9d59ec23d2f3a0dad25
JMSJoseph <jsecosky2@gmail.com>
2024-07-03 22:20:00 -0400

    Inital commit

---
+++ start.bat
@@ -0,0 +1,16 @@
+@echo off
+REM Check if the virtual environment directory exists
+if not exist venv (
+    echo Creating virtual environment...
+    py 3.11.8 -m venv venv
+)
+
+echo Activating virtual environment...
+call venv\Scripts\activate
+
+echo Installing dependencies...
+pip install --upgrade pip
+pip install -r requirements.txt
+
+echo Setup complete. Virtual environment is ready.
+pause
